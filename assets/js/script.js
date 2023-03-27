d5cf8bf3c1a2fa18940cb11cc7165e06c0aa0e62
ArturKljwa <kljwa1996@gmail.com>
2023-03-27 22:17:12 +0300

    feach: hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,23 @@ function open (evt) {
     tabTarget.classList.add('tabs__btn-item-active');
     document.querySelector(`#${button}`).classList.add('tabs__content_item_active')
 }
+
+// Hamburger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.navigation-mobile'),
+    menuItem = document.querySelectorAll('.navigation-mobile__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('navigation-mobile_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('navigation-mobile_active');
+        })
+    })
+})
+
